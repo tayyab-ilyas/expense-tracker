fb954fd175eb8886c070bf6896789df7dc11f688
Tayyab <tayyabilyas963@gmail.com>
2023-06-28 17:23:19 +0530

    style & separating comps

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,10 @@
+import ExpenseDate from "./ExpenseDate";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
-    const month= props.element.date.toLocaleString('en-US',{month:'long'});
-    const day= props.element.date.toLocaleString('en-US',{day:'2-digit'});
-    const year=props.element.date.getFullYear();
   return (
     <div className="expense-item">
-      <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
-      </div>
+      <ExpenseDate date={props.element.date}/>
       <div className="expense-item__description">
         <h2>{props.element.item}</h2>
         <div className="expense-item__price">${props.element.amount}</div>
