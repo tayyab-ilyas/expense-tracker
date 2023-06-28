57faf67289d9d4615f381d622988173fcf8e7d21
Tayyab <tayyabilyas963@gmail.com>
2023-06-28 12:27:27 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import ExpenseItem from "./components/ExpenseItem";
 
 function App(){
+  const expenses = [
+    {
+      item: 'Grocery',
+      date: new Date(26, 1, 2019),
+      amount: 100
+    },
+    {
+      item: 'Car',
+      date: new Date(1, 1, 2020),
+      amount: 2000
+    },
+    {
+    item: 'PS5',
+    date: new Date(2, 2, 2023),
+    amount: 1000
+    }
+  ];
   return(
     <div>
       <h1>Expense Tracker</h1>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem element={expenses[0]}/>
+      <ExpenseItem element={expenses[1]}/>
+      <ExpenseItem element={expenses[2]}/>
     </div>
   );
 }
--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,12 @@
 import "./ExpenseItem.css";
 
-function ExpenseItem() {
+function ExpenseItem(props) {
   return (
     <div className="expense-item">
-      <div>June 20th 2023</div>
+      <div>{props.element.date.toISOString()}</div>
       <div className="expense-item__description">
-        <h2>Grocery</h2>
-        <div className="expense-item__price">$599</div>
+        <h2>{props.element.item}</h2>
+        <div className="expense-item__price">${props.element.amount}</div>
       </div>
     </div>
   );
